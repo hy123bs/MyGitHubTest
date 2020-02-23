9f82d708f0b08a666442b27aacbdf6c2b5c11f33
hy123bs <hy123bs@gmail.com>
2020-02-23 14:20:33 +0800

    init setup

---
+++ app/src/main/java/tw/com/hy123/mygithubtest/MainActivity.java
@@ -0,0 +1,38 @@
+package tw.com.hy123.mygithubtest;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btntest;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btntest = findViewById(R.id.btnTest);
+
+
+        btntest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.i("aa","bb");
+
+
+
+            }
+        });
+
+
+
+
+
+    }
+}
